efb66c25c50f7c40b3508d460c3147519cf0bbb3
Alex Smith <alex_smith3@hotmail.com>
2017-10-17 12:34:19 +0100

    added css and add and remove owner

--- controllers/owner_controller.rb
+++ controllers/owner_controller.rb
@@ -9,7 +9,23 @@ get '/owners' do
   erb ( :"owners/index" )
 end
 
+get '/owners/new' do
+  erb(:"owners/new")
+end
+
 get '/owners/:id' do
   @owner = Owner.find(params['id'])
   erb(:"owners/show")
+end
+
+post '/owners' do
+ @owner = Owner.new(params)
+ @owner.save()
+ redirect to("/owners")
+end
+
+post '/owners/:id/delete' do
+  @owner = Owner.find(params['id'])
+  @owner.delete
+  redirect to("/owners")
 end
